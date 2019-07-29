08f86e0f01b320b2c558f9395f0d5e86a2db1c47
QUNLIN CHEN <39872236+cqllogic@users.noreply.github.com>
2019-07-29 15:29:17 -0400

    Update asy_node.m

--- asy_node.m
+++ asy_node.m
@@ -17,19 +17,19 @@ ns = size(lh_ts{1,1},2);
 for n = 1:length(lh_ts)
     % degree of node within left hemisphere
     CM_LL = corr(lh_ts{n,1});CM_LL(1:ns+1:end)=0;   
-    CM_LL = thres_absolute(fisherz(CM_LL),thres);
+    CM_LL = threshold_absolute(fisherz(CM_LL),thres);
     ALLsub_LL(n,:) = nansum(CM_LL);
     % LR >> degree of node corss hemisphere
     CM_LR = corr(lh_ts{n,1},rh_ts{n,1});
-    CM_LR = thres_absolute(fisherz(CM_LR),thres);
+    CM_LR = threshold_absolute(fisherz(CM_LR),thres);
     ALLsub_LR(n,:) = nansum(CM_LR');
     % RR >> degree of node within right hemisphere
     CM_RR = corr(rh_ts{n,1});CM_RR(1:ns+1:end)=0;   
-    CM_RR = thres_absolute(fisherz(CM_RR),thres);
+    CM_RR = threshold_absolute(fisherz(CM_RR),thres);
     ALLsub_RR(n,:) = nansum(CM_RR);
     % LR >> degree of node corss hemisphere
     CM_RL = corr(rh_ts{n,1},lh_ts{n,1}); 
-    CM_RL = thres_absolute(fisherz(CM_RL),thres);
+    CM_RL = threshold_absolute(fisherz(CM_RL),thres);
     ALLsub_RL(n,:) = nansum(CM_RL'); 
 end
 
